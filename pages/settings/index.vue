45e5dc16448f20b9484ac9cc1a2e263b2474698b
redgoose <scripter@me.com>
2018-02-09 09:01:14 +0900

    Settings 페이지 작업중..

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -1,17 +1,53 @@
 <template>
 <article class="contents settings">
-	<header class="contents__header settings__header">
+	<header class="contents__header">
 		<h1>Settings</h1>
 	</header>
 
 	<div class="contents__body">
-		<p>
-			// TODO: settings form
-		</p>
+		<section class="settings__section">
+			<header class="settings__sectionHeader">
+				<h1>qtum core</h1>
+				<p>The area that manages the core.</p>
+			</header>
+			<div class="form-kit">
+				<dl class="form-kit__horizontal">
+					<dt>Power ON/OFF</dt>
+					<dd>
+						<div>
+							// TODO: switch component
+						</div>
+						<p class="form-kit__description">
+							Warning message
+						</p>
+					</dd>
+				</dl>
+			</div>
+		</section>
 
-		<nav>
-			<button type="button" v-on:click="back">back</button>
-		</nav>
+		<section class="settings__section">
+			<header class="settings__sectionHeader">
+				<h1>General</h1>
+				<p>Basic setting area</p>
+			</header>
+			<form class="form-kit settings__form">
+				<dl class="form-kit__horizontal">
+					<dt>Theme</dt>
+					<dd>
+						<div>
+							// TODO: radio component
+						</div>
+					</dd>
+				</dl>
+				<nav class="form-kit__nav">
+					<button-basic
+						type="submit"
+						:label="false ? 'Processing..' : 'APPLY'"
+						:disabled="false"
+						className="button-key button-inline"/>
+				</nav>
+			</form>
+		</section>
 	</div>
 </article>
 </template>
@@ -19,17 +55,19 @@
 
 <script>
 import * as lib from '~/lib';
+import ButtonBasic from '~/components/button/button-basic';
 
 export default {
+	components: {
+		ButtonBasic,
+	},
+
 	head: {
 		title: lib.util.makeTitle('Settings')
 	},
 
 	methods: {
-		back: function(e)
-		{
-			this.$router.go(-1);
-		}
+
 	},
 }
 </script>
